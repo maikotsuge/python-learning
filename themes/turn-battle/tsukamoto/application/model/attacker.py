541ee75fcd64048ff1eb5c001a963d4ac2ac8b24
tsukamoto <kento.tsukamoto@samurai-soft.com>
2019-04-21 14:21:44 +0900

    マジックナンバーを定数化

--- themes/turn-battle/tsukamoto/application/model/attacker.py
+++ themes/turn-battle/tsukamoto/application/model/attacker.py
@@ -3,6 +3,10 @@ from util.random_util import hit_percentage
 
 
 class Attacker:
+    CRITICAL_RATE: int = 25 # クリティカル率[%]
+    CRITICAL_DAMAGE: int = 2 # クリティカル倍数[倍]
+    CRITICAL_EFFECT: str = "会心の一撃！" # クリティカル演出
+
     def __init__(self, ability: Ability) -> None:
         self.ability = ability
         self.before_hp = ability.hp
@@ -11,18 +15,20 @@ class Attacker:
 
     def __calc_damage(self, enemy_ability: Ability) ->int:
         # 自分の攻撃力 - 敵の防御力のダメージ！
-        # 最低でも1はダメージを与えられる
-        _damage = max(self.ability.attack - enemy_ability.defense, 1)
+        _damage = self.ability.attack - enemy_ability.defense
 
-        # クリティカルの時はダメージを2倍にする
-        return _damage * 2 if self._is_critical else _damage
+        # 最低でも1はダメージを与えられる
+        _damage = max(_damage, 1)
+
+        # クリティカルの時はダメージをN倍にする
+        return _damage * self.CRITICAL_DAMAGE if self._is_critical else _damage
 
     def __is_critical_hit(self) ->bool:
-        # 25%の確率でクリティカルになる
-        return hit_percentage(25)
+        # N%の確率でクリティカルになる
+        return hit_percentage(self.CRITICAL_RATE)
 
     def get_critical_effect(self) -> str:
-        return "会心の一撃！" if self._is_critical else ""
+        return self.CRITICAL_EFFECT if self._is_critical else ""
 
     def do_attack(self, enemy_ability: Ability) -> None:
         # クリティカル判定
